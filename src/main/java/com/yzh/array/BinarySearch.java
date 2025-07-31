ced07ea08f4a864704a8a21aa9f27ef11ca9b92f
zihengyuan <zihengyuan@tencent.com>
2025-07-31 14:51:53 +0800

    格式修改

--- src/main/java/com/yzh/array/BinarySearch.java
+++ src/main/java/com/yzh/array/BinarySearch.java
@@ -3,20 +3,28 @@ package com.yzh.array;
 /**
  * 704.二分查找
  * 给定一个n个元素有序的(升序)整型数组nums和一个目标值 target,写一个函数搜索nums中的target,如果目标值存在返回下标,否则返回 -1
+ * 关键词:有序数组、无重复元素
  */
 public class BinarySearch {
 
+    /**
+     * 二分查找
+     *
+     * @param nums 升序不重复的数组
+     * @param target 目标值
+     * @result 下标
+     */
     public int search(int[] nums, int target) {
         int low = 0;
         int high = nums.length - 1;
         while (low <= high) {
             int mid = (low + high) / 2;
-            if (nums[mid] == target) {
-                return mid;
+            if (nums[mid] > target) {
+                high = mid - 1;
             } else if (nums[mid] < target) {
                 low = mid + 1;
-            }  else {
-                high = mid - 1;
+            } else {
+                return mid;
             }
         }
         return -1;
